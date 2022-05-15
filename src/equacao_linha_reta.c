361336cc0fa31e319c79410b6aa2be61e58fb222
Mateus Camargo <mateuscamargo0@outlook.com.br>
2022-05-15 01:29:02 -0300

    func_equacao_reduzida_reta

--- src/equacao_linha_reta.c
+++ src/equacao_linha_reta.c
@@ -1,26 +1,52 @@
 #include<stdio.h>
 // y = b + mx
-       
+
+
+void * func_equacao_reduzida_reta(
+    int n, 
+    float matriz[2][2], 
+    float *b, 
+    float *m
+);
+
 
 void main(){
-    int n; 
-    
+    int n=2; 
+    float b, m;
     float x, y;
-    float m, b;
+    float matriz[2][2];
+    
+    for(int linha = 0; linha < n; linha++){
+        printf("------\n");
+        printf("x: ");
+        scanf("%f", &x);
 
-    float d;
-
-    float sumx=0,sumxsq=0;
-    float sumy=0,sumxy=0;
-
-    printf("Numeros para n:");
-    scanf("%d",&n);
-
-    for(int i=0;i<n;i++){
-        printf("\nx: ");
-        scanf("%f",&x);
         printf("y: ");
-        scanf("%f",&y);
+        scanf("%f", &y);
+
+        matriz[linha][0] = x; 
+        matriz[linha][1] = y;
+    }
+
+    func_equacao_reduzida_reta(n, matriz, &b, &m); 
+    printf("\nM=%.2f\tB=%.2f\n", m, b);
+}
+
+
+void * func_equacao_reduzida_reta(
+    int n, 
+    float matriz[2][2], 
+    float *b, 
+    float *m
+){
+    float x, y;
+    float coeficiente_angular;
+    float sumx=0, sumxsq=0;
+    float sumy=0, sumxy=0;
+
+    for(int linha = 0; linha < n; linha++){
+        x = matriz[linha][0];
+        y = matriz[linha][1];
 
         sumx = sumx + x;
         sumxsq = sumxsq + (x * x);
@@ -28,13 +54,8 @@ void main(){
         sumxy = sumxy + (x * y);
     }
 
-    d = n * sumxsq - sumx * sumx;
+    coeficiente_angular = n * sumxsq - sumx * sumx;
 
-    m = (n * sumxy - sumx * sumy) / d;
-    b = ( sumy * sumxsq - sumx * sumxy ) / d;
-
-    printf("\nM=%.2f\tB=%.2f\n", m, b);
-
+    *m = (n * sumxy - sumx * sumy) / coeficiente_angular;
+    *b = ( sumy * sumxsq - sumx * sumxy ) / coeficiente_angular;
 }
-
-
